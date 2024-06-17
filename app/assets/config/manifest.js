6be880b3a880ff52bd4fcb95f873a2f7fb721df4
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-06-17 09:37:15 +0300

    Remove the sprockets config

--- app/assets/config/manifest.js
+++
@@ -1,7 +0,0 @@
-// = link_tree ../images
-// = link_directory ../javascripts .js
-// = link ../javascripts/decidim/admin/color_field.js
-// = link ../javascripts/decidim/map/provider/helsinki.js
-// = link ../javascripts/helsinki/global-search.js
-// = link_directory ../javascripts/decidim/geocoding/provider .js
-// = link_directory ../stylesheets .css
