f9d5015ff00e130cd43acaed73b38252b9fe99f6
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2024-10-10 13:15:40 +0300

    Fix bug with the create blog post with the latest version
    In the current version the `attachment_attributes` method expects
    the `form` method to be accessible through `attr_reader`. Otherwise
    the wrong method is called.

--- app/commands/concerns/create_blog_post_overrides.rb
+++ app/commands/concerns/create_blog_post_overrides.rb
@@ -26,6 +26,9 @@ module CreateBlogPostOverrides
 
   private
 
+  # This is required for the `attachment_attributes` method to work correctly.
+  attr_reader :form
+
   def uploader_attributes
     attachment_attributes(:card_image, :main_image).delete_if { |_k, val| val.blank? }
   end
